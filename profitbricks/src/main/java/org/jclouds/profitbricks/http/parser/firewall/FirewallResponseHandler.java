cbd06564009a7215b61788d608553c0d287cc411
jasminSPC <jasmin@stackpointcloud.com>
2015-02-11 22:56:21 +0100

    Clean up

--- profitbricks/src/main/java/org/jclouds/profitbricks/http/parser/firewall/FirewallResponseHandler.java
+++ profitbricks/src/main/java/org/jclouds/profitbricks/http/parser/firewall/FirewallResponseHandler.java
@@ -1,11 +1,12 @@
 /*
- * Copyright 2015 The Apache Software Foundation.
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,11 +19,9 @@ package org.jclouds.profitbricks.http.parser.firewall;
 import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import java.util.List;
-import org.jclouds.date.DateCodecFactory;
 import org.jclouds.profitbricks.domain.Firewall;
 import org.jclouds.profitbricks.domain.FirewallRule;
 import org.jclouds.profitbricks.http.parser.firewallrule.FirewallRuleResponseHandler;
-import org.jclouds.profitbricks.http.parser.image.BaseImageResponseHandler;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 
